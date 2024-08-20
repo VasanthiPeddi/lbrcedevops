eb0d3dd4341a17cd876a393bb72ef93e2f89b3cd
AdmiN <AdmiN@ITWS0049>
2024-08-20 10:25:10 +0530

    commit

---
+++ src/lbrcedevops/Vassu.java
@@ -0,0 +1,11 @@
+package lbrcedevops;
+
+public class Vassu {
+
+	public static void main(String[] args) {
+		System.out.print("Hi Everyone, I am Vassu");
+		// TODO Auto-generated method stub
+
+	}
+
+}
